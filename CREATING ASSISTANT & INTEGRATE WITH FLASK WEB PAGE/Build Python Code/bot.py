7f477011f33cb1f231824f3f4020251ed9282481
Kowsalya-MK <90342844+Kowsalya-MK@users.noreply.github.com>
2022-11-07 18:07:32 +0530

    Add files via upload

---
+++ CREATING ASSISTANT & INTEGRATE WITH FLASK WEB PAGE/Build Python Code/bot.py
@@ -0,0 +1,22 @@
+#from flask import Flask, render_template
+#app = Flask(__name__)
+
+#@app.route('/')
+#def home():
+ #  return render_template('bot.html')
+#if __name__ == '__main__':
+ #  app.run(debug=TRUE)
+
+from flask import Flask,render_template
+
+app=Flask(__name__,template_folder='template')
+
+
+@app.route("/")
+def home():
+    return render_template('bot.html')
+
+
+
+if __name__=="__main__":
+    app.run(debug=True)
